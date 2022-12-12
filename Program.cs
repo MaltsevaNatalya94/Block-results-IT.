af8f7f5739446dc8731b864f03d852ec88cc2619
«Natalya» <MaltsevaNatalya94@yandex.ru>
2022-12-12 23:59:11 +0300

    program complete))

--- Program.cs
+++ Program.cs
@@ -6,6 +6,41 @@
 
 string[] array = mainArray();
 
+string[] result = resultArray(array, 3);
+
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] resultArray(string[] input, int n)
+{
+    string[] output = new string[counter(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int counter(string[] input, int n) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if (input[i].Length <= n) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
 string[] mainArray() 
 {
     Console.WriteLine("Введите значения ");
